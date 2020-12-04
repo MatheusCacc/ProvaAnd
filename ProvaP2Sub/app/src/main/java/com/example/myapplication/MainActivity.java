5dc7d7dffd6d70eff0d6b32826ef4d038259b9c3
MatheusCacc <matheusc.r@hotmail.com>
2020-12-03 21:28:28 -0400

    Final Commit

--- ProvaP2Sub/app/src/main/java/com/example/myapplication/MainActivity.java
+++ ProvaP2Sub/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     EditText Data;
     EditText RG;
     EditText Endereco;
+    ListView Ver;
+
+    public List<String> itens = new ArrayList<>();
 
 
     @Override
@@ -36,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         Data = findViewById(R.id.txtDate);
         RG = findViewById(R.id.txtRG);
         Endereco = findViewById(R.id.txtEnd);
+        Ver = findViewById(R.id.Ver);
 
         IniciarFirebase();
     }
@@ -64,9 +76,31 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Cadastrado com Sucesso", Toast.LENGTH_SHORT).show();
     }
 
+    public void CarregarListView()
+    {
+        reference = FirebaseDatabase.getInstance().getReference().child("Pessoa");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    Pessoa a = dataSnapshot1.getValue(Pessoa.class);
+                    itens.add("Nome:" + a.getNome() +", Data: " + a.getData()+", RG: " + a.getRG()+", Endereco: " + a.getEndereco());
+                }
+
+                ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                        getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        itens
+                );
+                Ver.setAdapter(adaptador);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
 
 
-
-
-
-}
+        }
